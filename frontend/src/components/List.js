89c67dbf6d26c59c2539b10666dbdcfb3077cfaf
feliarf <feliarisky23@gmail.com>
2019-12-10 12:56:14 +0700

    demo tutorial 8

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -5,7 +5,7 @@ export default function List({title, items, onItemClick, type}) {
     return (
         <>
             <h3 style={styles.heading}>{title}</h3>
-            <div className="list-group">
+            <div className="list-group" >
                 {items.map(item => (
                     <Item key={item.id} item={item} onChange={onItemClick} type={type}/>
                 ))}
